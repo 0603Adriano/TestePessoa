e3d43f78975f413cf3770330f6fe0d92ac11ad73
0603adriano <0603adriano@gmail.com>
2022-05-17 17:53:37 -0300

    Criada a classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,25 @@
-
 public class Pessoa {
-
+	
+	//Atributos
+	public String nome;
+	public int idade;
+	public double altura;
+	public double peso;
+	public String cpf;
+	public double imc;
+	
+	//Métodos
+	public double CalculaIMC() {
+		double calc = peso / (altura * altura);
+		return calc;
+	}
+	
+	public void MostraPessoa() {
+		String txt = "A pessoa: " + nome;
+		txt += "\n com idade: " + idade;
+		txt += "\n com peso: " + peso;
+		txt += "\n com altura: " + altura;
+		txt += "\n possui um IMC de : " + imc;
+		System.out.println(txt);
+	}
 }
